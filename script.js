a7c5c235e85fdbc9ec4d5243556a080560b3c61e
LucaLonga <31652369+LucaLonga@users.noreply.github.com>
2018-07-13 09:23:38 +0200

    ver3

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+/* parallasse*/
+<section class="slide" id="slide1" data-stellar-background-ratio="0.5">
+
+<section class="slide" id="slide3" data-stellar-background-ratio="0.5">
+
+<section class="slide" id="slide5" data-stellar-background-ratio="0.5">
+/* fine parallasse*/
